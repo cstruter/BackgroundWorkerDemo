b33ade1caaa0afcd23fa5642651f0e1ccee6c2c7
Christoff Truter <christoff@cstruter.com>
2013-01-21 18:52:01 +0200

    First version

---
+++ BackgroundWorkerDemo/mainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace BackgroundWorkerDemo
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lbxItems = new System.Windows.Forms.ListBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.cbxWorker = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(0, 102);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(42, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lbxItems
+            // 
+            this.lbxItems.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbxItems.FormattingEnabled = true;
+            this.lbxItems.Location = new System.Drawing.Point(0, 27);
+            this.lbxItems.Name = "lbxItems";
+            this.lbxItems.Size = new System.Drawing.Size(284, 69);
+            this.lbxItems.TabIndex = 2;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(48, 102);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(49, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // cbxWorker
+            // 
+            this.cbxWorker.AutoSize = true;
+            this.cbxWorker.Checked = true;
+            this.cbxWorker.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbxWorker.Location = new System.Drawing.Point(165, 106);
+            this.cbxWorker.Name = "cbxWorker";
+            this.cbxWorker.Size = new System.Drawing.Size(119, 17);
+            this.cbxWorker.TabIndex = 5;
+            this.cbxWorker.Text = "BackgroundWorker";
+            this.cbxWorker.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 129);
+            this.Controls.Add(this.cbxWorker);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lbxItems);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "Background Worker Demo";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ListBox lbxItems;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.CheckBox cbxWorker;
+    }
+}
+
---
+++ BackgroundWorkerDemo/mainForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace BackgroundWorkerDemo
+{
+    public partial class mainForm : Form
+    {
+        BackgroundWorker bw = new BackgroundWorker();
+
+        public mainForm()
+        {
+            InitializeComponent();
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            //bw.RunWorkerCompleted
+        }
+
+        void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lbxItems.Items.Insert(0, e.ProgressPercentage);
+        }
+
+        // Access controls residing in the main thread
+        void addItem(Int32 i)
+        {
+            if (lbxItems.InvokeRequired)
+            {
+                Action<Int32> a = new Action<int>(addItem);
+                this.BeginInvoke(a, i);
+            }
+            else
+            {
+                lbxItems.Items.Insert(0, i);
+            }
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Thread.Sleep(100);
+
+                if (bw.CancellationPending)
+                {
+                    e.Cancel = true;
+                }
+
+                if (!e.Cancel)
+                {
+                    //addItem(i);
+                    bw.ReportProgress(i);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (cbxWorker.Checked)
+            {
+                if (!bw.IsBusy)
+                {
+                    bw.RunWorkerAsync();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    Thread.Sleep(100);
+                    lbxItems.Items.Insert(0, i);
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            bw.CancelAsync();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
